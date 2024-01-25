5d338563d018bb6ce044166ccd697c3ed210ced1
sakaijyou <G0231228G@g.neecac.jp>
2024-01-25 11:20:30 +0900

    解像度の定義

---
+++ Chiritori/Screen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chiritori
+{
+    public static class Screen
+    {
+        public const int Width = 640;
+        public const int Height = 480;
+    }
+}

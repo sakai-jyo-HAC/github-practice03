0cf73230236c167dbb76356d45b51eb84002c221
sakaijyou <G0231228G@g.neecac.jp>
2024-01-25 11:39:46 +0900

    ゴミの作成

--- Chiritori/Game.cs
+++ Chiritori/Game.cs
@@ -7,12 +7,15 @@ namespace Chiritori
     public class Game
     {
         Chiritori chiritori;
+        Gomi gomi; // ゴミ
+
         public void Init()
         {
             Image.Load(); // 画像の読み込み
             MyRandomcs.Init(); // MyRandomの初期化
             Input.Init(); // Inputの初期化
             chiritori = new Chiritori(); // チリトリーの生成
+            gomi = new Gomi(); // ゴミの生成
         }
 
         public void Update()
@@ -24,6 +27,7 @@ namespace Chiritori
         public void Draw()
         {
             DX.DrawGraph(0, 0, Image.woodFloor); // 背景描画
+            gomi.Draw(); // ゴミ描画
             chiritori.Draw(); // チリトリー描画
         }
     }
---
+++ Chiritori/Gomi.cs
@@ -0,0 +1,35 @@
+﻿using DxLibDLL;
+using MyLib;
+using MyLIb;
+
+namespace Chiritori
+{
+    // ゴミクラス
+    public class Gomi
+    {
+        public float x; // x座標
+        public float y; // y座標
+
+        // コンストラクタ
+        public Gomi()
+        {
+            // 初期位置はランダム
+            ResetPosition();
+        }
+
+        // 場所をランダムにリセットする
+        public void ResetPosition()
+        {
+            x = MyRandomcs.Range(0, Screen.Width);
+            y = MyRandomcs.Range(0, Screen.Height);
+        }
+
+        // 描画処理
+        public void Draw()
+        {
+            // 別に回転させるわけではないが、横着してDrawRotaGraphF()を使っている。
+            // 勝手に中央揃えしてくれて楽だから。
+            DX.DrawRotaGraphF(x, y, 1f, 0f, Image.gomi);
+        }
+    }
+}
